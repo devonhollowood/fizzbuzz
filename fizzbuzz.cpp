7c1aa01199845b0fe6eb316c87a0ec5176a4bb42
Devon Hollowood <devonhollowood@gmail.com>
2015-05-11 23:46:13 -0400

    Got string_append and itoa working

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -1,14 +1,43 @@
-#include <array>
 #include <iostream>
 
 template <char... Cs>
-struct compile_time_string {
+struct string {
     constexpr static char value[] = {Cs..., '\0'};
 };
 
 template <char... Cs>
-constexpr char compile_time_string<Cs...>::value[];
+constexpr char string<Cs...>::value[];
+
+template <typename S, char C>
+struct string_append;
+
+template <char... Cs, char C>
+struct string_append<string<Cs...>, C> {
+    using result = string<Cs..., C>;
+};
+
+template <bool B, size_t N>
+struct itoa_helper;
+
+//recursive itoa_helper
+template <bool B, size_t N>
+struct itoa_helper {
+    using result = typename string_append<
+            typename itoa_helper<N/10<10, N/10>::result,
+            '0' + N%10
+        >::result;
+};
+
+template <size_t N>
+struct itoa_helper<true, N> {
+    using result = string<'0' + N>;
+};
+
+template <size_t N>
+struct itoa {
+    using result = typename itoa_helper<N<10, N>::result;
+};
 
 int main() {
-    std::cout << compile_time_string<'h', 'e', 'l', 'l', 'o'>::value << std::endl;
+    std::cout << itoa<24601>::result::value << std::endl;
 }
