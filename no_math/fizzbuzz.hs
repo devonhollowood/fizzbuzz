e01234d7c7b51a79f1c3358b72185dd2bf03b372
Devon Hollowood <devonhollowood@gmail.com>
2016-11-22 17:33:58 -0800

    Change no_math solution to allow for no "sounds"

--- no_math/fizzbuzz.hs
+++ no_math/fizzbuzz.hs
@@ -2,7 +2,8 @@ makeCycle :: (Int, String) -> [String]
 makeCycle sound = cycle $ replicate (fst sound - 1) "" ++ [snd sound]
 
 makeSounds :: [(Int, String)] -> [String]
-makeSounds = zipWith choose count . foldr1 (zipWith (++)) . map makeCycle
+makeSounds =
+    zipWith choose count . foldr (zipWith (++)) (repeat "") . map makeCycle
     where count = map show [1..]
           choose a b = if null b then a else b
 
